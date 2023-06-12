23bb5fa84cc81583cd2cefbb88fc95662857f019
IvanArrieta <2217940@ucc.edu.ar>
2023-06-12 08:20:28 -0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/trabajo_final.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Barco.cpp" "CMakeFiles/trabajo_final.dir/Barco.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Barco.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Barco1Casilla.cpp" "CMakeFiles/trabajo_final.dir/Barco1Casilla.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Barco1Casilla.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Barco2Casillas.cpp" "CMakeFiles/trabajo_final.dir/Barco2Casillas.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Barco2Casillas.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Barco3Casillas.cpp" "CMakeFiles/trabajo_final.dir/Barco3Casillas.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Barco3Casillas.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Barco4Casillas.cpp" "CMakeFiles/trabajo_final.dir/Barco4Casillas.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Barco4Casillas.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Juego.cpp" "CMakeFiles/trabajo_final.dir/Juego.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Juego.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/Tablero.cpp" "CMakeFiles/trabajo_final.dir/Tablero.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/Tablero.cpp.obj.d"
+  "C:/Users/Ivan/Desktop/prog/BatallaNaval/BatallaNaval/main.cpp" "CMakeFiles/trabajo_final.dir/main.cpp.obj" "gcc" "CMakeFiles/trabajo_final.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/trabajo_final.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabajo_final.dir/Barco.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Barco.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/Barco1Casilla.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Barco1Casilla.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/Barco2Casillas.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Barco2Casillas.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/Barco3Casillas.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Barco3Casillas.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/Barco4Casillas.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Barco4Casillas.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/Juego.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Juego.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/Tablero.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/Tablero.cpp.obj.d"
+  "CMakeFiles/trabajo_final.dir/main.cpp.obj"
+  "CMakeFiles/trabajo_final.dir/main.cpp.obj.d"
+  "libtrabajo_final.dll.a"
+  "trabajo_final.exe"
+  "trabajo_final.exe.manifest"
+  "trabajo_final.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabajo_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
